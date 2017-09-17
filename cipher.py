957973623abfe2cf4b27c376f9e999ded2501e8f
Stéphane Steffffff <Stphane Steffffff>
2017-09-17 23:51:40 +0200

    add somes caract

--- cipher.py
+++ cipher.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
+# -*- coding: cp1252 -*-
 import random
 class Cipher:
     
     def __init__(self):
-        self.alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-        self.alphabetTransition = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-        self.alphabetCipher = [""]*26
+        self.alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","é","à","ç","è","ê","â","ë","ä"," ","?",";",".",":","!","\"","'","0","1","2","3","4","5","6","7","8","9"]
+        self.alphabetTransition = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","é","à","ç","è","ê","â","ë","ä"," ","?",";",".",":","!","\"","'","0","1","2","3","4","5","6","7","8","9"]
+        self.alphabetCipher = [""]*78
         self.cipherText=""
     def cipher(self, text_):
         textCrypt = ""
-        text_ = text_.lower()
+       
         
         self.generateAlphabet()
         text_temp  =[]
@@ -23,7 +24,7 @@ class Cipher:
 
     def generateAlphabet(self):
         i=0
-        while i < 26:
+        while i < 78:
             y = random.randint(0,len(self.alphabetTransition)-1)
             self.alphabetCipher[i] = self.alphabetTransition[y]
             del self.alphabetTransition[y]
